10114669d4291443d628ed969f34458c129bd462
inoue, masatoshi <114462151+inomasa1013@users.noreply.github.com>
2023-06-01 19:07:42 +0900

    index.js
    Co-authored-by: inomasa1013 <inomasa1013@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -3,4 +3,3 @@ const knexConfig = require("./data/knexfile");
 const environment = process.env.DATABASE_URL ? "production" : "development";
 
 module.exports = knexConfig[environment];
-module.exports = knex(knexConfig[environment]);
