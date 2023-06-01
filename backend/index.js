cc3fed310dd5d75ba3f9ebd13c037110d1f61dd7
inoue, masatoshi <114462151+inomasa1013@users.noreply.github.com>
2023-06-01 18:31:48 +0900

    package.json to index.js
    Co-authored-by: inomasa1013 <inomasa1013@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const knex = require("knex");
-const knexConfig = require("./knexfile");
+const knexConfig = require("./data/knexfile");
 const environment = process.env.DATABASE_URL ? "production" : "development";
 
 module.exports = knex(knexConfig[environment]);
