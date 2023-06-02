d053de78481a09d6d30d98130c45ce37afdec0d5
inoue, masatoshi <114462151+inomasa1013@users.noreply.github.com>
2023-06-02 16:18:36 +0900

    push
    Co-authored-by: inomasa1013 <inomasa1013@users.noreply.github.com>

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -5,10 +5,10 @@ console.log(process.env.DATABASE_URL, "//", process.env);
 export default function Button(props) {
   const getAllPoke = async () => {
     let result = [];
-    console.log("ボタン押されたよ");
+    console.log("ボタン押された");
 
     const pokeArray = await fetch(
-      "https://sample-pokemon.onrender.com/api/poke" //
+      "https://sample-pokemon.onrender.com/api/poke"
     ); //.then((e) =>
     result = await pokeArray.json();
     props.resultNumSet(result);
