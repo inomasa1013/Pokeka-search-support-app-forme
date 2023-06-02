8c4365614cb206068cd04412a6f0dc07032fc026
inoue, masatoshi <114462151+inomasa1013@users.noreply.github.com>
2023-06-02 15:00:16 +0900

    build
    Co-authored-by: inomasa1013 <inomasa1013@users.noreply.github.com>

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.sendFile("./public/index.html");
+  res.sendFile("../frontend/build/index.html");
 });
 
 module.exports = router;
